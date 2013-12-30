2661a104c7878797ab994e109ffc2f47924bb846
Julian Ceballos <julian@pikhub.com>
2013-12-29 18:36:27 -0600

    settings import

--- impulso/urls.py
+++ impulso/urls.py
@@ -3,6 +3,7 @@
 
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
