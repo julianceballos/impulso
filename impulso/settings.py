47439f83eabf37473c4af72976c06366cd958dbc
Julian Ceballos <julian@pikhub.com>
2013-12-29 18:29:57 -0600

    done!

--- impulso/settings.py
+++ impulso/settings.py
@@ -69,7 +69,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = path('static')
+#STATIC_ROOT = path('static')
+STATIC_ROOT = "/root/impulso/admin/static/"
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
